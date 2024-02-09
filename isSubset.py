f1108accb26422001a43763ec7a86a78c24f1959
zbluee <99397149+zbluee@users.noreply.github.com>
2024-02-09 05:48:32 -0500

    Create isSubset.py

---
+++ isSubset.py
@@ -0,0 +1,41 @@
+#User function Template for python3
+from collections import Counter
+
+def isSubset( a1, a2, n, m):
+    count_a1 = Counter(a1)
+    
+    for a in a2:
+        if count_a1[a] == 0:
+            return "No"
+            
+        count_a1[a] -= 1
+    
+    return "Yes"
+    
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        sz = [int(x) for x in input().strip().split()]
+        n, m = sz[0], sz[1]
+        a1 = [int(x) for x in input().strip().split()]
+        a2 = [int(x) for x in input().strip().split()]
+        
+        print(isSubset( a1, a2, n, m))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
