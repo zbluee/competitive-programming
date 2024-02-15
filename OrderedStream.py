9089b79a277f3d83ee80c2b7d94ba28334680063
zbluee <99397149+zbluee@users.noreply.github.com>
2024-02-15 14:40:35 -0500

    Create OrderedStream.py

---
+++ OrderedStream.py
@@ -0,0 +1,22 @@
+class OrderedStream:
+
+    def __init__(self, n: int):
+        self.stream = [None] * (n+1)
+        self.size = n+1
+        self.ptr = 1
+
+    def insert(self, idKey: int, value: str) -> List[str]:
+        self.stream[idKey] = value
+        result = []
+        
+        if self.ptr == idKey:
+            while self.ptr < self.size and self.stream[self.ptr]:
+                result.append(self.stream[self.ptr])
+                self.ptr += 1
+        
+        return result  
+
+
+# Your OrderedStream object will be instantiated and called as such:
+# obj = OrderedStream(n)
+# param_1 = obj.insert(idKey,value)
